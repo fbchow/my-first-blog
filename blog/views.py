ec02deba5d7e19796d1835f3cdf1f384bcee5140
fbchow <fannybchow@gmail.com>
2015-12-25 18:17:26 -0800

    added additional post HTML

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.utils import timezone
+
+#add the Post model from the current directory
+from .models import Post
+
+
 
 #post_list function takes in request
 #returns render of the 'blog/post_list.html' template
 def post_list(request):
-	return render(request, 'blog/post_list.html', {})
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') 
+	return render(request, 'blog/post_list.html', {'posts': posts})
